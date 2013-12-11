400b1e04506151b3f9eff55273c65b7c5dacad74
Žilvinas Mikulis <zilvinas.mikulis@visma.com>
2013-12-11 11:32:23 +0200

    master komitas po rollbacko

--- EFTestConsole/Program.cs
+++ EFTestConsole/Program.cs
@@ -15,30 +15,31 @@ namespace EFTestConsole
             using (var rep = new CustomerRepository())
             {
                 Customer c1 = null;
-                {
-                    Id = rep.GenerateId(),
-                    ContactAddress = new Address()
-                    {
-                        City = "Wilno",
-                        Country = "LT",
-                        Street = "BBZ"
-                    },
-                    Name = "testas 2"
-                };
-                var ord = new Order(cst.Id, "11", 10)
-                {
-                    Id = rep.GenerateId()
-                };
-                cst.Orders.Add(ord);
-                var ord1 = new Order(cst.Id, "15", 25)
-                {
-                    Id = rep.GenerateId()
-                };
-                cst.Orders.Add(ord1);
-                rep.Insert(cst);
-                //c1 = rep.Get(20);
-                //var n2 = c1.Orders.Count;
-                //rep.Delete(20);
+            //    var cst = new Customer
+            //    {
+            //        Id = rep.GenerateId(),
+            //        ContactAddress = new Address()
+            //        {
+            //            City = "Wilno",
+            //            Country = "LT",
+            //            Street = "BBZ"
+            //        },
+            //        Name = "testas 2"
+            //    };
+            //    var ord = new Order(cst.Id, "11", 10)
+            //    {
+            //        Id = rep.GenerateId()
+            //    };
+            //    cst.Orders.Add(ord);
+            //    var ord1 = new Order(cst.Id, "15", 25)
+            //    {
+            //        Id = rep.GenerateId()
+            //    };
+            //    cst.Orders.Add(ord1);
+            //    rep.Insert(cst);
+                c1 = rep.Get(20);
+                var n2 = c1.Orders.Count;
+                rep.Delete(20);
                 rep.Save();
             }
         }
